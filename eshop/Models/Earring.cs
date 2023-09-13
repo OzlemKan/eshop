6f121cb929460600a46dff1445cafe401a50b89b
Pauline Neuville <pauline.neuville@gmail.com>
2023-09-13 16:21:41 +0200

    creation des models

---
+++ eshop/Models/Earring.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace test_2.Models;
+
+public class Earring
+{
+    
+    [Key]
+    public int EarringId { get; set; }
+            
+    public string Image { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public int Quantity { get; set; } 
+    public string Description { get; set; } // avec le type de métal, de pierre ainsi que l'entretien
+    public string Warranty { get; set; } // garantie
+    public string Expedition { get; set; } // livraison
+        
+}
---
+++ eshop/Models/Ring.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace test_2.Models;
+
+public class Ring
+{
+    [Key]
+    public int RingId { get; set; }
+            
+    public string Image { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public int Quantity { get; set; } 
+    public string Description { get; set; } // avec le type de métal, de pierre ainsi que l'entretien
+    public string Warranty { get; set; } // garantie
+    public string Expedition { get; set; } // livraison
+}
